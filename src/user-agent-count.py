f095250395ee79a7d4a847b62de2f37751a8d452
Hernán Vargas <hern.var@gmail.com>
2016-08-16 10:53:22 -0400

    src: agregado un contador de user-agent

---
+++ src/user-agent-count.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Example:
+#114.94.18.47<sep>20/Aug/2014:09:03:04 -0400<sep>PREFIX void: <http://rdfs.org/ns/void#>  PREFIX dv: <http://bio2rdf.org/bio2rdf.dataset_vocabulary:>  SELECT ?type (str(?count) AS ?count)  WHERE {   ?x void:subset [    a dv:Dataset-Type-Count;    void:class ?type;    void:entities ?count  ]}   ORDER BY DESC(?count) <sep>cu.linkedspl.bio2rdf.org<sep>Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36<sep>200<sep>643
+
+import sys
+
+if __name__ == '__main__':
+    dic = {}
+    file_list = sys.argv[1:]
+    for name in file_list:
+        with open(name) as f:
+            nline = 0
+            for line in f:
+                nline += 1
+                sp = line.split('<sep>')
+                try:
+                    user_agent = sp[4]
+                    if dic.has_key(user_agent):
+                        dic[user_agent] += 1
+                    else:
+                        dic[user_agent] = 1
+                except:
+                    print>>sys.stderr, name +'('+str(nline)+')', sp[-1][:-1]
+                    lfile = name
+    for key in dic:
+        print dic[key], '\t', key
