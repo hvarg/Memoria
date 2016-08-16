0bb8a20bbc857d5669219ad0da1e5875c52048fd
Hernán Vargas <hern.var@gmail.com>
2016-08-16 11:05:30 -0400

    src: Agregada recursividad

--- src/user-agent-count.py
+++ src/user-agent-count.py
@@ -4,11 +4,18 @@
 # Example:
 #114.94.18.47<sep>20/Aug/2014:09:03:04 -0400<sep>PREFIX void: <http://rdfs.org/ns/void#>  PREFIX dv: <http://bio2rdf.org/bio2rdf.dataset_vocabulary:>  SELECT ?type (str(?count) AS ?count)  WHERE {   ?x void:subset [    a dv:Dataset-Type-Count;    void:class ?type;    void:entities ?count  ]}   ORDER BY DESC(?count) <sep>cu.linkedspl.bio2rdf.org<sep>Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36<sep>200<sep>643
 
-import sys
+import sys, os
 
 if __name__ == '__main__':
     dic = {}
     file_list = sys.argv[1:]
+    # Recursivo
+    for f in file_list:
+        if os.path.isdir(f):
+            file_list.remove(f)
+            for new_file in os.listdir(f):
+                file_list.append(f + '/' + new_file)
+
     for name in file_list:
         with open(name) as f:
             nline = 0
