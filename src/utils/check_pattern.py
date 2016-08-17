70ab4825d6fc30ff00c78824196cf55a92558c91
Hernán Vargas <hern.var@gmail.com>
2016-08-17 16:09:44 -0300

    src: Arreglos a scripts para detectar patrones.

--- src/utils/check_pattern.py
+++ src/utils/check_pattern.py
@@ -5,33 +5,40 @@ import sys, json
 import numpy as np
 from datetime import datetime
 
+SESION_DUR = 100
+
 def diff_dates(date1, date2):
-  return abs((date2-date1).seconds)
+  return abs((date2-date1)).seconds
 
 if __name__ == '__main__':
   ps = {}
-  with open(sys.argv[1], 'r') as f:
-    for line in f:
-      js_d = json.loads(line)
-      if not ps.has_key(js_d['pattern']): 
-        ps[js_d['pattern']] = {}
-        ps[js_d['pattern']]['count']  = 0
-        ps[js_d['pattern']]['uris']   = {}
-        ps[js_d['pattern']]['vars']   = {}
-        ps[js_d['pattern']]['lits']   = {}
-        ps[js_d['pattern']]['dates']  = []
-      ps[js_d['pattern']]['count'] += 1
-      if not ps[js_d['pattern']]['uris'].has_key(str(js_d['uris'])):
-        ps[js_d['pattern']]['uris'][str(js_d['uris'])] = 0
-      if not ps[js_d['pattern']]['vars'].has_key(str(js_d['variables'])):
-        ps[js_d['pattern']]['vars'][str(js_d['variables'])] = 0
-      if not ps[js_d['pattern']]['lits'].has_key(str(js_d['literals'])):
-        ps[js_d['pattern']]['lits'][str(js_d['literals'])] = 0
-      ps[js_d['pattern']]['uris'][str(js_d['uris'])]      += 1
-      ps[js_d['pattern']]['vars'][str(js_d['variables'])] += 1
-      ps[js_d['pattern']]['lits'][str(js_d['literals'])]  += 1
-      ps[js_d['pattern']]['dates'].append(datetime.strptime(
-        js_d['date'][:-6], '%d/%b/%Y:%H:%M:%S')) #FIXME
+  for f in sys.argv[1:]:
+    with open(f, 'r') as f:
+      for line in f:
+        js_d = json.loads(line)
+        if not ps.has_key(js_d['pattern']): 
+          ps[js_d['pattern']] = {}
+          ps[js_d['pattern']]['count']  = 0
+          ps[js_d['pattern']]['uris']   = {}
+          ps[js_d['pattern']]['vars']   = {}
+          ps[js_d['pattern']]['lits']   = {}
+          ps[js_d['pattern']]['ua']     = {}
+          ps[js_d['pattern']]['dates']  = []
+        ps[js_d['pattern']]['count'] += 1
+        if not ps[js_d['pattern']]['uris'].has_key(str(js_d['uris'])):
+          ps[js_d['pattern']]['uris'][str(js_d['uris'])] = 0
+        if not ps[js_d['pattern']]['vars'].has_key(str(js_d['variables'])):
+          ps[js_d['pattern']]['vars'][str(js_d['variables'])] = 0
+        if not ps[js_d['pattern']]['lits'].has_key(str(js_d['literals'])):
+          ps[js_d['pattern']]['lits'][str(js_d['literals'])] = 0
+        if not ps[js_d['pattern']]['ua'].has_key(str(js_d['user-agent'])):
+          ps[js_d['pattern']]['ua'][str(js_d['user-agent'])] = 0
+        ps[js_d['pattern']]['uris'][str(js_d['uris'])]      += 1
+        ps[js_d['pattern']]['vars'][str(js_d['variables'])] += 1
+        ps[js_d['pattern']]['lits'][str(js_d['literals'])]  += 1
+        ps[js_d['pattern']]['ua'][str(js_d['user-agent'])]  += 1
+        ps[js_d['pattern']]['dates'].append(datetime.strptime(
+          js_d['date'][:-6], '%d/%b/%Y:%H:%M:%S')) #FIXME
   results = {}
   for key in ps:
     results[key] = []
@@ -42,19 +49,29 @@ if __name__ == '__main__':
     m = max([ps[key]['lits'][k] for k in ps[key]['lits']])
     results[key].append( float(m)/float(ps[key]['count']) )
     diffs = []
+    # Se calcula el tiempo entre consultas de las mismas sesiones, asumiendo que
+    # cuando pasa un tiempo mayor a una hora son sesiones diferentes.
     last  = None
     for date in ps[key]['dates']:
       if last != None:
-        diffs.append( diff_dates(date, last) )
+        rest = diff_dates(date, last)
+        if rest < SESION_DUR:
+         diffs.append( rest )
       last = date
-    arr = np.array( diffs )
-    results[key].append( np.mean(arr) )
-    results[key].append( np.std(arr) )
+    if len(diffs) > 1:
+      arr = np.array( diffs )
+      results[key].append( np.mean(arr) )
+      results[key].append( np.std(arr) )
+    else:
+      results[key].append(0)
+      results[key].append(0)
 
   for key in ps:
     print "%d ocurrencias del patron: %s" % (ps[key]['count'], key)
     print "\tUn %0.2f%% utilizó las mismas URIs" % (results[key][0] *100)
     print "\tUn %0.2f%% utilizó las mismas variables" % (results[key][1] *100)
     print "\tUn %0.2f%% utilizó los mismos literales" % (results[key][2] *100)
-    print "\tLa media del tiempo transcurrido entre consultas fue de",
-    print "%0.2f segundos (ds = %0.2f)" % tuple(results[key][3:])
+    if results[key][3] != 0:
+      print "\tLa media del tiempo transcurrido entre consultas fue de",
+      print "%0.2f segundos (ds = %0.2f)" % tuple(results[key][3:])
+    print "\tSe utilizaron %d user-agent diferentes." % (len(ps[key]['ua']))
--- src/utils/create_json_log.py
+++ src/utils/create_json_log.py
@@ -8,8 +8,7 @@ Genera un diccionario con los datos y patrones de las consultas.
 
 Opciones:
     -d                   Debug.
-    -r                   Recursivo.
-    --one-file           Guarda todos los resultados en un solo archivo.
+    --debug              Recursivo.
     --indent             Guarda los resultados con indentacion. 
     -h, --help           Muestra esta ayuda y termina."""
 
@@ -17,6 +16,7 @@ Opciones:
 
 re_limit   = re.compile(r'limit (\d+)', re.IGNORECASE)
 re_offset  = re.compile(r'offset (\d+)', re.IGNORECASE)
+re_prefix  = re.compile(r'prefix ([a-zA-Z0-9_]+:) (<[^<> ]*>)', re.IGNORECASE)
 re_uri     = re.compile(r'(<[^<> ]*>)')
 re_literal = re.compile(r'("[^"]*"|\d+|' + r"'[^']*')")
 re_var     = re.compile(r'\?[a-zA-Z0-9_]+')
@@ -37,8 +37,19 @@ def get_pattern(raw):
       else: c -= 1
     i -= 1
   if (i == 0): IndexError("No se encontro WHERE.")
-  # Determinando variables
   pattern = raw[init:end]
+  if not pattern:
+    pattern = raw #test
+  # Reemplazando prefijos
+  prefixes = re_prefix.findall(raw[:init])
+  if prefixes:
+    for p, ur in prefixes:
+      l = [m.end(0) for m in re.finditer(p, pattern)]
+      for x in l:
+        while pattern[x] not in [' ',')','}']: x += 1
+        pattern = pattern[:x] + '>' + pattern[x:]
+      pattern = re.sub(p, ur[:-1], pattern)
+  # Determinando variables
   uris = re_uri.findall(pattern)
   if uris: pattern = re_uri.sub('_URI_', pattern)
   vars = re_var.findall(pattern)
@@ -57,21 +68,19 @@ def get_pattern(raw):
 if __name__ == '__main__':
   rec      = False
   debug    = False
-  one_file = False
   indent   = False
 
   try:
-    options, files = getopt.gnu_getopt(sys.argv[1:], 'hdr', 
-        ["help", "one-file", "indent"])
+    options, files = getopt.gnu_getopt(sys.argv[1:], 'hr', 
+        ["help", "debug", "indent"])
   except Exception,e:
     print str(e)
     exit(-1)
 
   for opt, arg in options:
     if   opt in ("--help", "-h"):   print help_string; exit(0)
-    elif opt == "--one-file":       one_file  = True
     elif opt == "--indent":         indent    = True
-    elif opt == "-d":               debug     = True
+    elif opt == "--debug":          debug     = True
     elif opt == "-r":               rec       = True
     
   if rec:
@@ -80,9 +89,8 @@ if __name__ == '__main__':
         for new_file in os.listdir(f):
           files.append(f + '/' + new_file)
 
-  dic = {}
-
   for f in files:
+    dic = {}
     if os.path.isdir(f): continue
     try:
       l = open(f, 'r')
@@ -103,21 +111,20 @@ if __name__ == '__main__':
           current['date']       = date
           current['raw']        = raw
           current['user-agent'] = user_agent
-          dic[ip].append(current)
           current['pattern'], current['uris'], current['variables'], \
               current['literals'], current['limit'], current['offset'] = \
               get_pattern(raw)
+          if current['pattern']: dic[ip].append(current)
+          else:
+            print>>sys.stderr, "%s (%d): consulta sin Where." % (f, nline)
         nline += 1
       if debug: print '\r'
       l.close()
-  if one_file:
-    with open('results.json', 'w') as out:
-      if indent: print>>out, json.dumps(dic, indent=2)
-      else:      print>>out, json.dumps(dic)
-  else:
+
     for ip in dic:
-      with open(ip + '.json', 'w') as out:
-        if indent: print>>out, json.dumps(dic[ip], indent=2)
-        else:
-          for query in dic[ip]:
-            print>>out, json.dumps(query)
+      if len(dic[ip]) > 0:
+        with open(ip + '.json', 'a') as out:
+          if indent: print>>out, json.dumps(dic[ip], indent=2)
+          else:
+            for query in dic[ip]:
+              print>>out, json.dumps(query)
